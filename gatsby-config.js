71ebd2b9db2c30d62d69385b66b4d892a179372f
panasheMuriro <panashemuriro2021@gmail.com>
2022-05-27 15:54:12 +0300

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,19 +20,23 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `mugove grading`,
+        short_name: `mugove`,
         start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#008080`,
+        theme_color: `#008080`,
+        display: `standalone`,
+        icon: `src/images/pwa_logo.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/index/`, `/components/UI/*`, `/fonts/*`, `/images/*`, `/static/*`, `/styles/*`, `/src/*`, `/classes/*`],
+        workboxConfig: {
+          globPatterns: ["**/*.{js,jpg,ttf,html,css,png}"],
+        },
+      },
+    },
   ],
 }
